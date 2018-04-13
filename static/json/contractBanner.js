570ef35076c968dce55cfdbae1c1fde10bd9383b
vincent <541890086@qq.com>
2018-04-13 10:51:01 +0800

    sixth change path

--- static/json/contractBanner.js
+++ static/json/contractBanner.js
@@ -1,5 +1,5 @@
 [
-  {"src":"/static/img/guntu4.jpg","id":"3"},
-  {"src":"/static/img/guntu5.jpg","id":"5"},
-  {"src":"/static/img/guntu6.jpg","id":"2"}
+  {"src":"./static/img/guntu4.jpg","id":"3"},
+  {"src":"./static/img/guntu5.jpg","id":"5"},
+  {"src":"./static/img/guntu6.jpg","id":"2"}
 ]
--- static/json/indexBanner.js
+++ static/json/indexBanner.js
@@ -1,5 +1,5 @@
 [
-  {"src":"/static/img/ABUIABACGAAgtPfy1AUo-Ii_KjDABzjCAw.jpg"},
-  {"src":"/static/img/65bb23d72c47fc5a9b16d2efac9c37c4.jpg"},
-  {"src":"/static/img/d1388fb97cd5d2970b918cd582fa32a6.jpg"}
+  {"src":"./static/img/ABUIABACGAAgtPfy1AUo-Ii_KjDABzjCAw.jpg"},
+  {"src":"./static/img/65bb23d72c47fc5a9b16d2efac9c37c4.jpg"},
+  {"src":"./static/img/d1388fb97cd5d2970b918cd582fa32a6.jpg"}
 ]
